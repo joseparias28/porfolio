4def8d7bd7600805b98e0e605c5b5b3625122744
Jose Arias <jarias@alumnat.copernic.cat>
2025-11-08 18:44:35 +0100

    Corregir configuración para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://joseparias28.github.io',
-  base: '/porfolio',
+  site: 'https://joseparias28.github.io/porfolio',
+  base: '/porfolio/',
 });
