ce40b1b3fdb2785ecdf1327d2c795997038684ce
Jose Arias <jarias@alumnat.copernic.cat>
2026-03-10 22:11:51 +0100

    Corregir rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://joseparias28.github.io/porfolio',
-  base: '/porfolio/',
+  site: 'https://porfolio.joseparias.com',
+  base: '/',
 });
